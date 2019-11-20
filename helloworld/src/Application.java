1ed33dccf4f1ee9be675be62100c4f39470b4dc3
Arvind <Arvind@192.168.0.122>
2019-11-19 23:06:49 -0800

    review

---
+++ helloworld/src/Application.java
@@ -0,0 +1,14 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+		Person p1 = new Person();
+		p1.name = "Bob";
+		p1.getAge(9);
+
+	}
+
+}
---
+++ helloworld/src/Person.java
@@ -0,0 +1,11 @@
+
+public class Person {
+	int age;
+	String name;
+	
+	int getAge(int age) {
+		System.out.println(name+" is "+ age+ " years old.");
+		return age;
+		
+		
+	}}
